f974d67572d384cdfcc5e8ee5318bc6e2cbbaa0e
javierkiu <javier28420@gmail.com>
2024-07-31 16:19:32 -0500

    Inline Temp

--- src/EmpleadoPorHoras.java
+++ src/EmpleadoPorHoras.java
@@ -15,8 +15,7 @@ public class EmpleadoPorHoras extends Empleado {
 
     @Override
     public double calcularSalario() {
-        double salario = super.getHorasTrabajadas() * super.getTarifaHora();
-        return salario;
+        return super.getHorasTrabajadas() * super.getTarifaHora();
     }
 
     // Más metodos
